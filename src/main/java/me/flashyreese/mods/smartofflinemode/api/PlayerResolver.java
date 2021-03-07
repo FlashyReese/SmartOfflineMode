85467f9e1634dbcc79902eb3a4baeaddd4f02637
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2021-03-07 01:32:12 -0600

    change: Use Mojang's endpoints for account checking

--- src/main/java/me/flashyreese/mods/smartofflinemode/api/PlayerResolver.java
+++ src/main/java/me/flashyreese/mods/smartofflinemode/api/PlayerResolver.java
@@ -2,94 +2,103 @@ package me.flashyreese.mods.smartofflinemode.api;
 
 import com.google.gson.Gson;
 import com.mojang.authlib.GameProfile;
+import it.unimi.dsi.fastutil.longs.Long2BooleanOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Object2BooleanOpenHashMap;
 
+import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 import java.util.UUID;
 
 public class PlayerResolver {
 
-    //Todo: Should use mojang's endpoint but only supports name, not uuid
-    public static final String API_ENDPOINT = "https://playerdb.co/api/player/minecraft";
+    private static final Long2BooleanOpenHashMap playerCacheByUUID = new Long2BooleanOpenHashMap();
+    private static final Object2BooleanOpenHashMap<String> playerCacheByName = new Object2BooleanOpenHashMap<>();
 
-    public static Optional<Result> findPlayer(String nameOrUUID) {
+    public static final String API_ENDPOINT = "https://api.mojang.com/users/profiles/minecraft";
+    public static final String SESSION_ENDPOINT = "https://sessionserver.mojang.com/session/minecraft/profile";
+
+    public static Optional<Result> getPlayer(String name) {
+        return getResult(API_ENDPOINT, name);
+    }
+
+    public static Optional<Result> getPlayer(UUID uuid) {
+        return getResult(SESSION_ENDPOINT, uuid.toString());
+    }
+
+    public static Optional<Result> getResult(String endPoint, String string) {
         Optional<Result> optionalResult;
         try {
-            URL url = new URL(String.format("%s/%s", API_ENDPOINT, nameOrUUID));
-            URLConnection urlConnection = url.openConnection();
-            urlConnection.addRequestProperty("User-Agent", "Mozilla");
+            URL url = new URL(String.format("%s/%s", endPoint, string));
+            HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
             urlConnection.setReadTimeout(5000);
             urlConnection.setConnectTimeout(5000);
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), StandardCharsets.UTF_8))) {
                 optionalResult = Optional.of(new Gson().fromJson(reader, Result.class));
-            }catch (Exception e){
+            } catch (Exception e) {
                 //e.printStackTrace();
                 optionalResult = Optional.empty();
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             optionalResult = Optional.empty();
         }
         return optionalResult;
     }
 
-    public static boolean isOnlineAccount(String nameOrUUID) {
-        Optional<Result> optionalResult = findPlayer(nameOrUUID);
-        if (optionalResult.isPresent()){
-            Result result = optionalResult.get();
-            return result.success;
+    public static boolean isOnlineAccount(String name) {
+        if (playerCacheByName.containsKey(name)){
+            return playerCacheByName.getBoolean(name);
+        } else {
+            Optional<Result> optionalResult = getPlayer(name);
+            if (optionalResult.isPresent()){
+                boolean isOnline = optionalResult.get().error == null;
+                playerCacheByName.put(name, isOnline);
+                return isOnline;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isOnlineAccount(UUID uuid) {
+        long uuidLong = uuid.getMostSignificantBits();
+        if (playerCacheByUUID.containsKey(uuidLong)){
+            return playerCacheByUUID.get(uuidLong);
+        } else {
+            Optional<Result> optionalResult = getPlayer(uuid);
+            if (optionalResult.isPresent()){
+                boolean isOnline = optionalResult.get().error == null;
+                playerCacheByUUID.put(uuidLong, isOnline);
+                return isOnline;
+            }
         }
         return false;
     }
 
     public static boolean isOnlineAccount(GameProfile gameProfile) {
-        return (gameProfile.getName() != null && isOnlineAccount(gameProfile.getName())) || (gameProfile.getId() != null && isOnlineAccount(gameProfile.getId().toString()));
-    }
-
-    public static UUID getUUID(String name) {
-        Optional<Result> optionalResult = findPlayer(name);
-        if (optionalResult.isPresent()){
-            Result result = optionalResult.get();
-            if (result.success) {
-                return UUID.fromString(result.data.player.id);
-            }
-        }
-        return UUID.nameUUIDFromBytes(String.format("OfflinePlayer:%s", name).getBytes(StandardCharsets.UTF_8));
+        return (gameProfile.getName() != null && isOnlineAccount(gameProfile.getName())) || (gameProfile.getId() != null && isOnlineAccount(gameProfile.getId()));
     }
 
     public static GameProfile getGameProfile(String name) {
-        Optional<Result> optionalResult = findPlayer(name);
-        if (optionalResult.isPresent()){
+        Optional<Result> optionalResult = getPlayer(name);
+        if (optionalResult.isPresent()) {
             Result result = optionalResult.get();
-            if (result.success) {
-                return new GameProfile(UUID.fromString(result.data.player.id), name);
+            if (result.error == null) {
+                return new GameProfile(UUID.fromString(result.id), name);
             }
         }
         return new GameProfile(null, name);
     }
 
-    private static class Result{
-        private String code;
-        private String message;
-        private ResultData data;
-        private boolean success;
-        private boolean error;
-    }
-
-    private static class ResultData{
-        private ResultDataPlayer player;
-    }
-
-    private static class ResultDataPlayer{
-        //Missing meta
-        private String username;
+    private static class Result {
+        private String name;
         private String id;
-        private String raw_id;
-        private String avatar;
+        private String error;
+        private String errorMessage;
     }
 }
